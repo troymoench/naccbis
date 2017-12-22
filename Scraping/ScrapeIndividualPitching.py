d79e4916f67510c13a54680b9b37a7c1328b6664
troymoench <brewersarenumber1@gmail.com>
2017-12-22 15:26:02 -0600

    Added IndvidualPitchingScraper

---
+++ Scraping/ScrapeIndividualPitching.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import psycopg2
+import sys
+import ScrapeFunctions as sf
+
+YEAR = "2016-17"
+OUTPUT = "csv"
+
+
+class IndividualPitchingScraper:
+    BASE_URL = "http://naccsports.org/sports/bsb/"  # add constants to ScrapeFunctions.py?
+    PITCHING_COLS = ['No.', 'Name', 'yr', 'pos', 'app', 'gs', 'w', 'l', 'ip', 'h', 'r', 'er',
+                    'era']
+    COACHES_VIEW_COLS = ['No.', 'Player', 'ERA', 'W', 'L', 'APP', 'GS', 'IP', 'H', 'R', 'ER', '2B', '3B', 'HR', 'AB']
+    TEAM_IDS = {
+        'Aurora': 'AUR',
+        'Benedictine': 'BEN',
+        'Concordia Chicago': 'CUC',
+        'Concordia Wisconsin': 'CUW',
+        'Dominican': 'DOM',
+        'Edgewood': 'EDG',
+        'Lakeland': 'LAK',
+        'MSOE': 'MSOE',
+        'Marian': 'MAR',
+        'Maranatha': 'MARN',
+        'Rockford': 'ROCK',
+        'Wisconsin Lutheran': 'WLC'
+    }
+    TABLES = {"overall": "raw_pitchers_overall"}
+
+    def __init__(self, year, output, verbose=False):
+        self._year = year
+        self._output = output
+        self._verbose = verbose
+        self._data = pd.DataFrame()
+        self._runnable = True
+
+    def info(self):
+        print("Individual Pitching Scraper")
+        print("Year:", self._year)
+        print("Output format:", self._output)
+        if self._runnable:
+            print("Scraper has not been run yet. Use run() to do so.")
+        else:
+            print("Scraper has been run")
+            print(self._data.info())
+
+    def run(self):
+        # run the scraper
+        # TODO: add argument export=True
+
+
+    def _scrape(self):
+        print("Scrape")
+
+    def _clean(self):
+        print("Clean")
+
+    def export(self):
+        print("Export")
+
+
+# ***********************************
+# ****** BEGINNING OF SCRIPT ********
+# ***********************************
+if __name__ == "__main__":
+    scraper = IndividualPitchingScraper(YEAR, OUTPUT, verbose=True)
+    scraper.info()
+    scraper.run()
+    # scraper.export()
+
