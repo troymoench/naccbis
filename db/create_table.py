e2f999c32478ba619bbc7201f735dd74d1067857
troymoench <brewersarenumber1@gmail.com>
2017-12-25 16:06:52 -0600

    Consolidated CREATE TABLE scripts

---
+++ db/create_table.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+# This script reads in an sql file with the DDL for a new table
+# The sql file name is stored in the fname variable
+
+conn = psycopg2.connect(host="192.168.0.101", database="naccbisdb", user="troy", password="baseballisfun")
+cur = conn.cursor()
+
+# might need to change numeric to double
+
+fname = "raw_team_pitching_overall.sql"
+with open(fname, 'r') as f:
+    query = f.read()
+
+print(query)
+
+cur.execute(query)
+
+conn.commit()
+
+cur.close()
+conn.close()
