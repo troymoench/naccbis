015d242edf96de48db962146bed6e4538343e970
Troy Moench <brewersarenumber1@gmail.com>
2018-09-19 23:00:29 -0500

    Pulled Levenshtein analysis into a function

--- Cleaning/DumpNames.py
+++ Cleaning/DumpNames.py
@@ -13,11 +13,56 @@ from Levenshtein import distance
 import CleanFunctions
 
 
+def levenshtein_analysis(data, levenshtein_first, levenshtein_last):
+    """ Perform a Levenshtein analysis on first names and last names.
+    This is used for identifying typos.
+    :param data: A DataFrame
+    :param levenshtein_first: Levenshtein distance between first names
+    :param levenshtein_last: Levenshtein distance between last names
+    :returns: A DataFrame with player-season pairs that meet the filter parameters
+    """
+    # Get unique last names
+    names = pd.DataFrame(data["lname"].unique())
+    names["key"] = 0
+    # print(names)
+
+    # Compute the cartesian product
+    cart_prod = pd.merge(names, names, on="key")
+    cart_prod.drop(columns=["key"], inplace=True)
+    cart_prod.columns = ["lname1", "lname2"]
+    # print(cart_prod)
+
+    # names.drop(columns=["key"], inplace=True)
+    # names.columns = ["lname"]
+
+    # Compute the Levenshtein distance between each pair of names
+    cart_prod["levenshtein"] = list(map(distance, cart_prod["lname1"], cart_prod["lname2"]))
+    # cart_prod = cart_prod[(cart_prod.levenshtein == 1) | (cart_prod.levenshtein == 2)]
+    cart_prod = cart_prod[cart_prod.levenshtein == levenshtein_last]
+
+    output = pd.merge(data, cart_prod, left_on="lname", right_on="lname1")
+
+    output = pd.merge(output, data, left_on="lname2", right_on="lname", how="inner")
+
+    output["levenshtein_first"] = list(map(distance, output["fname_x"], output["fname_y"]))
+    output = output[output["levenshtein_first"] == levenshtein_first]
+
+    # output = output[output["fname_x"] == output["fname_y"]]
+    output = output.sort_values(by=["levenshtein", "lname1"])
+    return output
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="DumpNames.py")
     parser.add_argument("-c", "--corrections", action="store_true",
                         help="Apply existing name corrections")
+    parser.add_argument("-f", "--fname", type=int, metavar="FNAME",
+                        help="Filter first names by a Levenshtein distance")
+    parser.add_argument("-l", "--lname", type=int, metavar="LNAME",
+                        help="Filter last names by a Levenshtein distance")
     args = parser.parse_args()
+    levenshtein_first = args.fname
+    levenshtein_last = args.lname
 
     with open('../config.json') as f:
         config = json.load(f)
@@ -50,27 +95,5 @@ if __name__ == "__main__":
         data = CleanFunctions.apply_corrections(data, corrections, verbose=True)
         data = data.sort_values(by=["lname", "fname", "team", "season"])
 
-    # Get unique last names
-    names = pd.DataFrame(data["lname"].unique())
-    names["key"] = 0
-    # print(names)
-
-    # Compute the cartesian product
-    cart_prod = pd.merge(names, names, on="key")
-    cart_prod.drop(columns=["key"], inplace=True)
-    cart_prod.columns = ["lname1", "lname2"]
-    # print(cart_prod)
-
-    # names.drop(columns=["key"], inplace=True)
-    # names.columns = ["lname"]
-
-    # Compute the Levenshtein distance between each pair of names
-    cart_prod["levenshtein"] = list(map(distance, cart_prod["lname1"], cart_prod["lname2"]))
-    cart_prod = cart_prod[(cart_prod.levenshtein == 1) | (cart_prod.levenshtein == 2)]
-    output = pd.merge(data, cart_prod, left_on="lname", right_on="lname1")
-
-    output = pd.merge(output, data, left_on="lname2", right_on="lname", how="inner")
-
-    output = output[output["fname_x"] == output["fname_y"]]
-    output = output.sort_values(by=["levenshtein", "lname1"])
-    output.to_csv("leven_last.csv", index=False)
+    output = levenshtein_analysis(data, levenshtein_first, levenshtein_last)
+    output.to_csv("output.csv", index=False)
