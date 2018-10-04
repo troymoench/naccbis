af54fcd4494036bf8ddfcf78d24965f84b56edb2
Troy Moench <brewersarenumber1@gmail.com>
2018-10-03 22:17:13 -0500

    Added unit test for normalize_names

--- Cleaning/tests.py
+++ Cleaning/tests.py
@@ -2,6 +2,7 @@
 
 import unittest
 import CleanFunctions as cf
+import pandas as pd
 
 
 class TestCleanFunctions(unittest.TestCase):
@@ -15,9 +16,15 @@ class TestCleanFunctions(unittest.TestCase):
     def test_add_n(self):
         self.assertEqual(cf.add_n("engelcu01", 2), "engelcu03")
 
-    # def test_normalize_names(self):
-    #     pass
-    #
+    def test_normalize_names(self):
+        df1 = pd.DataFrame({"name": ["Jeffrey  Mayes", "D.J.  Dillon", "Quinlan Milne Rojek"],
+                            "team": ["AUR", "BEN", "DOM"],
+                            "season": [2017, 2017, 2016]}, columns=["name", "team", "season"])
+        df2 = pd.DataFrame(df1)
+        df2["fname"] = ["Jeffrey", "DJ", "Quinlan"]
+        df2["lname"] = ["Mayes", "Dillon", "Milne Rojek"]
+        self.assertTrue(cf.normalize_names(df1).equals(df2))
+
     # def test_apply_corrections(self):
     #     pass
 
