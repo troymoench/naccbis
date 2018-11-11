cb4e6df8ca51a6c4ea65b0c96438c9c29e3558e7
Troy Moench <brewersarenumber1@gmail.com>
2018-11-11 16:31:25 -0600

    CleanTeamOffense.py now loads data into database table

--- Cleaning/CleanTeamOffense.py
+++ Cleaning/CleanTeamOffense.py
@@ -14,12 +14,18 @@ OUTPUT = "csv"
 if __name__ == "__main__":
     with open('../config.json') as f:
         config = json.load(f)
+    utils.init_logging()
 
     conn = utils.connect_db(config)
     data = pd.read_sql_table("raw_team_offense_{}".format(SPLIT), conn)
-    conn.close()
 
-    data.rename(columns={"name": "team"}, inplace=True)
+    # data.rename(columns={"name": "team"}, inplace=True)
 
     data = metrics.basic_offensive_metrics(data)
-    print(data)
+    columns = ["name", "season", "g", "pa", "ab", "r", "h", "x2b", "x3b", "hr", "rbi",
+               "bb", "so", "hbp", "tb", "xbh", "sf", "sh", "gdp", "sb", "cs", "go", "fo",
+               "go_fo", "hbp_p", "bb_p", "so_p", "iso", "babip", "avg", "obp", "slg",
+               "ops", "sar"]
+
+    data[columns].to_sql("team_offense_{}".format(SPLIT), conn, if_exists="append", index=False)
+    conn.close()
