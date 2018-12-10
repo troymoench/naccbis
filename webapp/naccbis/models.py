964ee0e67110c1714b359b0f8593cf7bc488ebd6
Troy Moench <brewersarenumber1@gmail.com>
2018-12-09 19:45:38 -0600

    Added more models and refactored views to use pandas

--- webapp/naccbis/models.py
+++ webapp/naccbis/models.py
@@ -8,6 +8,194 @@
 from django.db import models
 
 
+class BattersOverall(models.Model):
+    no = models.IntegerField(blank=True, null=True)
+    fname = models.CharField(primary_key=True, max_length=20)
+    lname = models.CharField(max_length=20)
+    team = models.CharField(max_length=5)
+    season = models.IntegerField()
+    yr = models.CharField(max_length=2, blank=True, null=True)
+    pos = models.CharField(max_length=15, blank=True, null=True)
+    g = models.IntegerField(blank=True, null=True)
+    pa = models.IntegerField(blank=True, null=True)
+    ab = models.IntegerField(blank=True, null=True)
+    r = models.IntegerField(blank=True, null=True)
+    h = models.IntegerField(blank=True, null=True)
+    x2b = models.IntegerField(blank=True, null=True)
+    x3b = models.IntegerField(blank=True, null=True)
+    hr = models.IntegerField(blank=True, null=True)
+    rbi = models.IntegerField(blank=True, null=True)
+    bb = models.IntegerField(blank=True, null=True)
+    so = models.IntegerField(blank=True, null=True)
+    hbp = models.IntegerField(blank=True, null=True)
+    tb = models.IntegerField(blank=True, null=True)
+    xbh = models.IntegerField(blank=True, null=True)
+    sf = models.IntegerField(blank=True, null=True)
+    sh = models.IntegerField(blank=True, null=True)
+    gdp = models.IntegerField(blank=True, null=True)
+    sb = models.IntegerField(blank=True, null=True)
+    cs = models.IntegerField(blank=True, null=True)
+    go = models.IntegerField(blank=True, null=True)
+    fo = models.IntegerField(blank=True, null=True)
+    go_fo = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    hbp_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    bb_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    so_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    babip = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    iso = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    avg = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    obp = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    slg = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    ops = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    sar = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'batters_overall'
+        unique_together = (('fname', 'lname', 'team', 'season'),)
+
+
+class DuplicateNames(models.Model):
+    fname = models.CharField(primary_key=True, max_length=20)
+    lname = models.CharField(max_length=20)
+    team = models.CharField(max_length=5)
+    season = models.IntegerField()
+    id = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'duplicate_names'
+        unique_together = (('fname', 'lname', 'team', 'season'),)
+
+
+class GameLog(models.Model):
+    game_num = models.IntegerField(primary_key=True)
+    date = models.DateField(blank=True, null=True)
+    season = models.IntegerField()
+    team = models.CharField(max_length=30)
+    opponent = models.CharField(max_length=30, blank=True, null=True)
+    result = models.CharField(max_length=1, blank=True, null=True)
+    rs = models.IntegerField(blank=True, null=True)
+    ra = models.IntegerField(blank=True, null=True)
+    home = models.BooleanField(blank=True, null=True)
+    conference = models.BooleanField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'game_log'
+        unique_together = (('game_num', 'season', 'team'),)
+
+
+class LeagueOffenseOverall(models.Model):
+    season = models.IntegerField(primary_key=True)
+    g = models.IntegerField(blank=True, null=True)
+    pa = models.IntegerField(blank=True, null=True)
+    ab = models.IntegerField(blank=True, null=True)
+    r = models.IntegerField(blank=True, null=True)
+    h = models.IntegerField(blank=True, null=True)
+    x2b = models.IntegerField(blank=True, null=True)
+    x3b = models.IntegerField(blank=True, null=True)
+    hr = models.IntegerField(blank=True, null=True)
+    rbi = models.IntegerField(blank=True, null=True)
+    bb = models.IntegerField(blank=True, null=True)
+    so = models.IntegerField(blank=True, null=True)
+    sb = models.IntegerField(blank=True, null=True)
+    cs = models.IntegerField(blank=True, null=True)
+    hbp = models.IntegerField(blank=True, null=True)
+    sf = models.IntegerField(blank=True, null=True)
+    sh = models.IntegerField(blank=True, null=True)
+    tb = models.IntegerField(blank=True, null=True)
+    xbh = models.IntegerField(blank=True, null=True)
+    gdp = models.IntegerField(blank=True, null=True)
+    go = models.IntegerField(blank=True, null=True)
+    fo = models.IntegerField(blank=True, null=True)
+    avg = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    obp = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    slg = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    ops = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    go_fo = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    hbp_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    bb_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    so_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    iso = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    babip = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    sar = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    lg_r_pa = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    bsr_bmult = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    bsr = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    lw_hbp = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    lw_bb = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    lw_x1b = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    lw_x2b = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    lw_x3b = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    lw_hr = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    lw_sb = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    lw_cs = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    lw_out = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    ww_hbp = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    ww_bb = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    ww_x1b = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    ww_x2b = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    ww_x3b = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    ww_hr = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    woba_scale = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    woba = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    sbr = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    lg_wsb = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    wsb = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    wraa = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    off = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    wrc = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    wrc_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    off_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    rep_level = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    rar = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'league_offense_overall'
+
+
+class NameCorrections(models.Model):
+    uc_fname = models.CharField(primary_key=True, max_length=20)
+    uc_lname = models.CharField(max_length=20)
+    uc_team = models.CharField(max_length=5)
+    uc_season = models.IntegerField()
+    c_fname = models.CharField(max_length=20)
+    c_lname = models.CharField(max_length=20)
+    type = models.CharField(max_length=1, blank=True, null=True)
+    submitted = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'name_corrections'
+        unique_together = (('uc_fname', 'uc_lname', 'uc_team', 'uc_season'),)
+
+
+class Nicknames(models.Model):
+    rid = models.IntegerField(blank=True, null=True)
+    name = models.CharField(primary_key=True, max_length=20)
+    nickname = models.CharField(max_length=20)
+
+    class Meta:
+        managed = False
+        db_table = 'nicknames'
+        unique_together = (('name', 'nickname'),)
+
+
+class PlayerId(models.Model):
+    fname = models.CharField(primary_key=True, max_length=20)
+    lname = models.CharField(max_length=20)
+    team = models.CharField(max_length=5)
+    season = models.IntegerField()
+    player_id = models.CharField(max_length=10, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'player_id'
+        unique_together = (('fname', 'lname', 'team', 'season'),)
+
+
 class RawBattersConference(models.Model):
     no = models.IntegerField(blank=True, null=True)
     name = models.CharField(primary_key=True, max_length=35)
@@ -40,9 +228,6 @@ class RawBattersConference(models.Model):
     go = models.IntegerField(blank=True, null=True)
     fo = models.IntegerField(blank=True, null=True)
     go_fo = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-
-    def __str__(self):
-        return "{} {} {} {}".format(self.no, self.name, self.team, self.season)
 
     class Meta:
         managed = False
@@ -122,12 +307,6 @@ class RawBattersOverall(models.Model):
     go = models.IntegerField(blank=True, null=True)
     fo = models.IntegerField(blank=True, null=True)
     go_fo = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
-
-    def calc_ops(self):
-        # NOTE: need to create a custom manager to append this to the QuerySet with annotate()
-        if self.obp is None or self.slg is None:
-            self.ops = None
-        self.ops = self.obp + self.slg
 
     class Meta:
         managed = False
@@ -828,6 +1007,55 @@ class RawTeamPitchingOverallInseason(models.Model):
         unique_together = (('name', 'season', 'date'),)
 
 
+class ReplacementLevel(models.Model):
+    season = models.IntegerField(primary_key=True)
+    g = models.IntegerField(blank=True, null=True)
+    pa = models.IntegerField(blank=True, null=True)
+    ab = models.IntegerField(blank=True, null=True)
+    r = models.IntegerField(blank=True, null=True)
+    h = models.IntegerField(blank=True, null=True)
+    x2b = models.IntegerField(blank=True, null=True)
+    x3b = models.IntegerField(blank=True, null=True)
+    hr = models.IntegerField(blank=True, null=True)
+    rbi = models.IntegerField(blank=True, null=True)
+    bb = models.IntegerField(blank=True, null=True)
+    so = models.IntegerField(blank=True, null=True)
+    hbp = models.IntegerField(blank=True, null=True)
+    tb = models.IntegerField(blank=True, null=True)
+    xbh = models.IntegerField(blank=True, null=True)
+    sf = models.IntegerField(blank=True, null=True)
+    sh = models.IntegerField(blank=True, null=True)
+    gdp = models.IntegerField(blank=True, null=True)
+    sb = models.IntegerField(blank=True, null=True)
+    cs = models.IntegerField(blank=True, null=True)
+    go = models.IntegerField(blank=True, null=True)
+    fo = models.IntegerField(blank=True, null=True)
+    avg = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    obp = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    slg = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    ops = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    go_fo = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    hbp_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    bb_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    so_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    iso = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    babip = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    sar = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    sbr = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    wsb = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    woba = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    wraa = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    off = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    wrc = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    wrc_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    off_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    off_pa = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'replacement_level'
+
+
 # class TeamIds(models.Model):
 #     name = models.CharField(max_length=30, blank=True, null=True)
 #     id = models.CharField(max_length=5, blank=True, null=True)
@@ -835,3 +1063,45 @@ class RawTeamPitchingOverallInseason(models.Model):
 #     class Meta:
 #         managed = False
 #         db_table = 'team_ids'
+
+
+class TeamOffenseOverall(models.Model):
+    name = models.CharField(primary_key=True, max_length=30)
+    season = models.IntegerField()
+    g = models.IntegerField(blank=True, null=True)
+    pa = models.IntegerField(blank=True, null=True)
+    ab = models.IntegerField(blank=True, null=True)
+    r = models.IntegerField(blank=True, null=True)
+    h = models.IntegerField(blank=True, null=True)
+    x2b = models.IntegerField(blank=True, null=True)
+    x3b = models.IntegerField(blank=True, null=True)
+    hr = models.IntegerField(blank=True, null=True)
+    rbi = models.IntegerField(blank=True, null=True)
+    bb = models.IntegerField(blank=True, null=True)
+    so = models.IntegerField(blank=True, null=True)
+    hbp = models.IntegerField(blank=True, null=True)
+    tb = models.IntegerField(blank=True, null=True)
+    xbh = models.IntegerField(blank=True, null=True)
+    sf = models.IntegerField(blank=True, null=True)
+    sh = models.IntegerField(blank=True, null=True)
+    gdp = models.IntegerField(blank=True, null=True)
+    sb = models.IntegerField(blank=True, null=True)
+    cs = models.IntegerField(blank=True, null=True)
+    go = models.IntegerField(blank=True, null=True)
+    fo = models.IntegerField(blank=True, null=True)
+    go_fo = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    hbp_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    bb_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    so_p = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    babip = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    iso = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    avg = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    obp = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    slg = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    ops = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    sar = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'team_offense_overall'
+        unique_together = (('name', 'season'),)
--- webapp/naccbis/views.py
+++ webapp/naccbis/views.py
@@ -1,28 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import RawBattersOverall
-
+from naccbis.models import BattersOverall
+import pandas as pd
 
 # Create your views here.
 def index(request):
 
-    data = RawBattersOverall.objects.using('data').filter(season=2018)
+    data = BattersOverall.objects.using('data').filter(season=2018)
     data = data.filter(pa__gt=100)
 
-    for row in data:
-        row.calc_ops()
+    # convert to DataFrame
+    fields = [field.name for field in data.model._meta.fields]
+    values = [row.__dict__ for row in data]
+    df = pd.DataFrame.from_records(values, columns=fields, coerce_float=True)
+    df.fillna(value=pd.np.nan, inplace=True)
+    # print(df)
 
-    print(request.GET.get("sort"))
-    sort_param = request.GET.get("sort")
-    # print(sort_param.split(","))
-    if sort_param is not None:
-        # data = data.order_by("-pa")
-        descending = True
-        if len(sort_param.split(",")) > 1:
-            descending = sort_param.split(",")[1] != "a"
-        data = sorted(data, key=lambda x: getattr(x, sort_param.split(",")[0]), reverse=descending)
-    else:
-        data = sorted(data, key=lambda x: x.ops, reverse=True)
+    # sort
+    df.sort_values(by=["ops"], ascending=False, inplace=True)
+    df.reset_index(drop=True, inplace=True)
 
-    context = {'data': data}
+    # print(request.GET.get("sort"))
+    # sort_param = request.GET.get("sort")
+    # # print(sort_param.split(","))
+    # if sort_param is not None:
+    #     # data = data.order_by("-pa")
+    #     descending = True
+    #     if len(sort_param.split(",")) > 1:
+    #         descending = sort_param.split(",")[1] != "a"
+    #     data = sorted(data, key=lambda x: getattr(x, sort_param.split(",")[0]), reverse=descending)
+    # else:
+    #     data = sorted(data, key=lambda x: x.ops, reverse=True)
+
+    # round float columns
+    df = df.round({"go_fo": 2, "sar": 2, "hbp_p": 1, "bb_p": 1, "so_p": 1,
+                   "babip": 3, "iso": 3, "avg": 3, "obp": 3, "slg": 3, "ops": 3})
+    # rename / select columns based on stat view
+    df.columns = [col.upper() for col in df.columns.tolist()]
+    df.rename(columns={"GO_FO": "GO/FO", "HBP_P": "HBP%", "BB_P": "BB%", "SO_P": "SO%",
+                       "X2B": "2B", "X3B": "3B"}, inplace=True)
+
+    context = {'data': df.to_html(classes="table table-bordered table-hover table-sm",
+                                  index=True, na_rep="")}
     return render(request, 'naccbis/index.html', context)
