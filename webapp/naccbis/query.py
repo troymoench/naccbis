c7c061ce68ae7c5a60546b0e0225743edbafa003
Troy Moench <brewersarenumber1@gmail.com>
2018-12-15 15:18:20 -0600

    PandasQuerySet now uses model._meta.get_fields()

--- webapp/naccbis/query.py
+++ webapp/naccbis/query.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 class PandasQuerySet(query.QuerySet):
     def as_dataframe(self):
-        fields = [field.name for field in self.model._meta.fields]
+        fields = [field.name for field in self.model._meta.get_fields()]
         values = [row.__dict__ for row in self]
         df = pd.DataFrame.from_records(values, columns=fields, coerce_float=True)
         df.fillna(value=pd.np.nan, inplace=True)
