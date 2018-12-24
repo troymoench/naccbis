d8e43c53f17007e29e9c3d15b6a5de1f4e8fe825
Troy Moench <brewersarenumber1@gmail.com>
2018-12-24 15:33:31 -0600

    Refactored CleanTeamPitching.py into a class

--- Cleaning/CleanTeamPitching.py
+++ Cleaning/CleanTeamPitching.py
@@ -11,15 +11,38 @@ import metrics
 SPLIT = "overall"
 
 
+class TeamPichingCleaner:
+    """ ETL class for team pitching """
+    VALID_SPLITS = ["overall", "conference"]
+
+    def __init__(self, split, conn):
+        if split not in self.VALID_SPLITS:
+            raise ValueError("Invalid split: {}".format(split))
+        self.split = split
+        self.conn = conn
+
+    def extract(self):
+        self.data = pd.read_sql_table("raw_team_pitching_{}".format(self.split), self.conn)
+
+    def transform(self):
+        self.data["ip"] = self.data["ip"].apply(cf.convert_ip)
+        conference = (self.split == "conference")
+        self.data = metrics.basic_pitching_metrics(self.data, conference=conference)
+
+    def load(self):
+        self.data.to_sql("team_pitching_{}".format(self.split), self.conn, if_exists="append", index=False)
+
+    def run(self):
+        self.extract()
+        self.transform()
+        self.load()
+
+
 if __name__ == "__main__":
     with open('../config.json') as f:
         config = json.load(f)
     utils.init_logging()
-
     conn = utils.connect_db(config)
-    data = pd.read_sql_table("raw_team_pitching_{}".format(SPLIT), conn)
-    data["ip"] = data["ip"].apply(cf.convert_ip)
-    data = metrics.basic_pitching_metrics(data, conference=(SPLIT == "conference"))
-    # print(data.info())
-    data.to_sql("team_pitching_{}".format(SPLIT), conn, if_exists="append", index=False)
+    cleaner = TeamPichingCleaner(SPLIT, conn)
+    cleaner.run()
     conn.close()
