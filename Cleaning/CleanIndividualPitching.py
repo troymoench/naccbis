b1c0f3215f96133bf3d8a657974e74483439d502
Troy Moench <brewersarenumber1@gmail.com>
2018-12-24 16:16:15 -0600

    Refactored CleanIndividualPitching.py into a class

--- Cleaning/CleanIndividualPitching.py
+++ Cleaning/CleanIndividualPitching.py
@@ -5,38 +5,61 @@ import json
 import pandas as pd
 # Local imports
 import CleanFunctions as cf
-import utils
 import metrics
+import utils
 
 SPLIT = "overall"
+
+
+class IndividualPitchingCleaner:
+    """ ETL class for individual pitching """
+    VALID_SPLITS = ["overall", "conference"]
+
+    def __init__(self, split, conn):
+        if split not in self.VALID_SPLITS:
+            raise ValueError("Invalid split: {}".format(split))
+        self.split = split
+        self.conn = conn
+
+    def extract(self):
+        self.data = pd.read_sql_table("raw_pitchers_{}".format(self.split), self.conn)
+        self.corrections = pd.read_sql_table("name_corrections", self.conn)
+
+    def transform(self):
+        self.data = cf.normalize_names(self.data)
+        self.data = cf.apply_corrections(self.data, self.corrections)
+        self.data.drop(columns=["name"], inplace=True)
+
+        self.data["ip"] = self.data["ip"].apply(cf.convert_ip)
+        conference = (self.split == "conference")
+        self.data = metrics.basic_pitching_metrics(self.data, conference=conference)
+        self.data.replace(pd.np.inf, pd.np.nan, inplace=True)
+        if self.split == "overall":
+            columns = ['no', 'fname', 'lname', 'team', 'season', 'yr', 'pos', 'g', 'gs',
+                       'w', 'l', 'sv', 'cg', 'sho', 'ip', 'h', 'r', 'er', 'bb', 'so',
+                       'x2b', 'x3b', 'hr', 'ab', 'wp', 'hbp', 'bk', 'sf', 'sh', 'pa',
+                       'hbp_p', 'bb_p', 'so_p', 'iso', 'babip', 'avg', 'obp', 'slg', 'ops',
+                       'lob_p', 'era', 'ra_9', 'so_9', 'bb_9', 'hr_9', 'whip']
+        if self.split == "conference":
+            columns = ['no', 'fname', 'lname', 'team', 'season', 'yr', 'pos', 'g', 'gs',
+                       'w', 'l', 'sv', 'cg', 'ip', 'h', 'r', 'er', 'bb', 'so', 'so_9',
+                       'hr', 'era', 'ra_9', 'bb_9', 'hr_9', 'whip']
+        self.data = self.data[columns]
+
+    def load(self):
+        self.data.to_sql("pitchers_{}".format(self.split), conn, if_exists="append", index=False)
+
+    def run(self):
+        self.extract()
+        self.transform()
+        self.load()
 
 
 if __name__ == "__main__":
     with open('../config.json') as f:
         config = json.load(f)
-
+    utils.init_logging()
     conn = utils.connect_db(config)
-    data = pd.read_sql_table("raw_pitchers_{}".format(SPLIT), conn)
-    corrections = pd.read_sql_table("name_corrections", conn)
-
-    data = cf.normalize_names(data)
-    data = cf.apply_corrections(data, corrections)
-    data.drop(columns=["name"], inplace=True)
-
-    data["ip"] = data["ip"].apply(cf.convert_ip)
-    data = metrics.basic_pitching_metrics(data, conference=(SPLIT == "conference"))
-    data.replace(pd.np.inf, pd.np.nan, inplace=True)
-    if SPLIT == "overall":
-        columns = ['no', 'fname', 'lname', 'team', 'season', 'yr', 'pos', 'g', 'gs',
-                   'w', 'l', 'sv', 'cg', 'sho', 'ip', 'h', 'r', 'er', 'bb', 'so',
-                   'x2b', 'x3b', 'hr', 'ab', 'wp', 'hbp', 'bk', 'sf', 'sh', 'pa',
-                   'hbp_p', 'bb_p', 'so_p', 'iso', 'babip', 'avg', 'obp', 'slg', 'ops',
-                   'lob_p', 'era', 'ra_9', 'so_9', 'bb_9', 'hr_9', 'whip']
-    if SPLIT == "conference":
-        columns = ['no', 'fname', 'lname', 'team', 'season', 'yr', 'pos', 'g', 'gs',
-                   'w', 'l', 'sv', 'cg', 'ip', 'h', 'r', 'er', 'bb', 'so', 'so_9',
-                   'hr', 'era', 'ra_9', 'bb_9', 'hr_9', 'whip']
-    data = data[columns]
-    # print(data.info())
-    data.to_sql("pitchers_{}".format(SPLIT), conn, if_exists="append", index=False)
+    cleaner = IndividualPitchingCleaner(SPLIT, conn)
+    cleaner.run()
     conn.close()
