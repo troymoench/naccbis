94c3f8d2325f78239d983c99e54ed82aac167694
Troy Moench <brewersarenumber1@gmail.com>
2019-10-13 15:11:33 -0500

    Updated docs version

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,9 +20,9 @@ copyright = '2019, Troy Moench'
 author = 'Troy Moench'
 
 # The short X.Y version
-version = ''
+version = '0.1'
 # The full version, including alpha/beta/rc tags
-release = '0.0'
+release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
