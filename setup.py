4c08e5db46108d866fef3cb8df4599e869a9c672
Troy Moench <brewersarenumber1@gmail.com>
2019-11-23 13:31:59 -0600

    Update version to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from glob import glob
 
 setup(
     name="naccbis",
-    version="0.1",
+    version="0.2",
     packages=find_packages(exclude=["scripts"]),
     scripts=glob('naccbis/scripts/*.py'),
 )
