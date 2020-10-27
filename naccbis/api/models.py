34cdf83163e5c47e95c06dd1d75348ff50b966c4
Troy Moench <brewersarenumber1@gmail.com>
2020-10-26 20:17:15 -0500

    Added naming convention to api models

--- naccbis/api/models.py
+++ naccbis/api/models.py
@@ -5,6 +5,15 @@ from sqlalchemy.sql import func
 from .database import Base
 
 
+Base.metadata.naming_convention = {
+  "ix": "ix_%(column_0_label)s",
+  "uq": "uq_%(table_name)s_%(column_0_name)s",
+  "ck": "ck_%(table_name)s_%(constraint_name)s",
+  "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+  "pk": "pk_%(table_name)s"
+}
+
+
 class PlayerId(Base):
     __tablename__ = "player_id"
 
