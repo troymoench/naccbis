4a1566d334b30f5053d517066f08afeb46bd367d
Troy Moench <brewersarenumber1@gmail.com>
2022-01-23 17:00:45 -0600

    Added some data to game_log api test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,9 @@
 from fastapi.testclient import TestClient
 import pytest
+from sqlalchemy import delete
 from naccbis.api.main import app, get_db
 from naccbis.api.database import create_session
+from naccbis.common.models import GameLog
 from naccbis.common.settings import Settings
 
 NACCBIS_DB_URL = "postgresql:///naccbisdb_test"
@@ -26,6 +28,12 @@ def client():
     return TestClient(app)
 
 
+@pytest.fixture
+def db():
+    with TestingSessionLocal() as session:
+        yield session
+
+
 def test_ping(client):
     response = client.get("/ping")
     assert response.status_code == 200
@@ -39,10 +47,51 @@ def test_player(client):
     print(response.json())
 
 
-def test_game_log(client):
+def test_game_log(client, db):
+    game_log1 = GameLog(
+        game_num=25,
+        date="2018-04-21",
+        season=2018,
+        team="Wisconsin Lutheran",
+        opponent="Dominican",
+        result="W",
+        rs=14,
+        ra=1,
+        home=False,
+        conference=True,
+    )
+    game_log2 = GameLog(
+        game_num=15,
+        date="2018-04-08",
+        season=2018,
+        team="Edgwood",
+        opponent="Wisconsin Lutheran",
+        result="L",
+        rs=3,
+        ra=4,
+        home=True,
+        conference=True,
+    )
+    db.add_all([game_log1, game_log2])
+    db.commit()
     response = client.get(
         "/game_log/",
         params={"team": "Wisconsin Lutheran", "season": 2018},
     )
     assert response.status_code == 200
-    assert response.json() == []
+    assert response.json() == [
+        {
+            "game_num": 25,
+            "date": "2018-04-21",
+            "season": 2018,
+            "team": "Wisconsin Lutheran",
+            "opponent": "Dominican",
+            "result": "W",
+            "rs": 14,
+            "ra": 1,
+            "home": False,
+            "conference": True,
+        }
+    ]
+    db.execute(delete(GameLog))
+    db.commit()
