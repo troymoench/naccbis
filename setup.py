ff6e54293ebec04993562b617016ddb8f4e84013
Troy Moench <brewersarenumber1@gmail.com>
2022-11-11 01:32:42 -0600

    Converted setup.py to declarative metadata using setup-py-upgrade and setup-cfg-fmt

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-import os
-import re
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-with open(os.path.join(os.path.dirname(__file__), "naccbis", "__init__.py")) as v_file:
-    VERSION = (
-        re.compile(r""".*__version__ = ["'](.*?)['"]""", re.S)
-        .match(v_file.read())
-        .group(1)
-    )
-
-setup(
-    name="naccbis",
-    version=VERSION,
-    packages=find_packages(exclude=["tests"]),
-    entry_points={
-        "console_scripts": [
-            "naccbis = naccbis.scripts.cli:cli",
-        ]
-    },
-    python_requires=">=3.9",
-)
+setup()
